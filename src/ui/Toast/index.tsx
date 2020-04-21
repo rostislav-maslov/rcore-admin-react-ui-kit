feec6414e6d95f5e125609b64559cd8e615fd969
arendoid <obstructeddoodle@protonmail.com>
2020-04-21 12:42:07 +0400

    Базовая реализация компонента Тоста

---
+++ src/ui/Toast/index.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import MuiAlert, { AlertProps } from "@material-ui/lab/Alert";
+import { Snackbar, makeStyles } from "@material-ui/core";
+
+type Props = {
+  message: string;
+  isOpen: boolean;
+  onClickHandle: (e?: any) => void;
+};
+
+function Alert(props: AlertProps) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+export const Toast: React.FC<Props> = ({ isOpen, message, onClickHandle }) => {
+  const classes = useStyles();
+  const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
+    // if (reason === 'clickaway') {
+    //   return;
+    // }
+    onClickHandle(false);
+  };
+
+  return (
+    <Snackbar open={isOpen} onClose={handleClose}>
+      <Alert onClose={handleClose}>{message}</Alert>
+    </Snackbar>
+  );
+};
+
+const useStyles = makeStyles({
+  root: {
+    width: "100%",
+  },
+});
+
+// TODO styles
