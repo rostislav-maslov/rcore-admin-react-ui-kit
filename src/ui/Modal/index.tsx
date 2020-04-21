d9b7fbd88d266dd71932beb09a4b2c88fd68c186
arendoid <obstructeddoodle@protonmail.com>
2020-04-21 12:43:58 +0400

    Базовая реализация компонента Модальное окно

---
+++ src/ui/Modal/index.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import { Modal as MuiModal, ModalProps } from "@material-ui/core";
+
+type Props = {
+  open: boolean;
+} & ModalProps;
+
+export const Modal: React.FC<Props> = ({ open, children, ...other }) => {
+  return <MuiModal open={open}>{children}</MuiModal>;
+};
