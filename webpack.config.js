ddafac3e747697aa50a82ac11807bcb74e029af9
arendoid <obstructeddoodle@protonmail.com>
2020-05-06 14:05:00 +0400

    configure webpack & tsconfig a minor improvement

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+
+module.exports = {
+  mode: 'production',
+  entry: ['./dist/index.js'],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'index.js',
+  },
+  resolve: {
+    modules: ['node_modules'],
+    alias: {
+      'react-dom': '@hot-loader/react-dom',
+    },
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(j|t)s(x)?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true,
+            babelrc: false,
+            presets: [
+              [
+                '@babel/preset-env',
+                { targets: { browsers: 'last 2 versions' } }, // or whatever your project requires
+              ],
+              '@babel/preset-typescript',
+              '@babel/preset-react',
+            ],
+            plugins: ['react-hot-loader/babel'],
+          },
+        },
+      },
+    ],
+  },
+  devtool: 'source-map',
+  plugins: [new ForkTsCheckerWebpackPlugin()],
+}
